1d72232a72facaac67537e1714d13275be28d00a
Donald Park <donaldjpark@gmail.com>
2019-10-31 20:12:09 -0700

    update

--- public/assets/js/article.js
+++ public/assets/js/article.js
@@ -1,8 +1,24 @@
 $.getJSON("/articles", function(data) {
     // For each one
-    for (var i = 0; i < data.length; i++) {
+    for (var i = 0; i < 10; i++) {
       // Display the apropos information on the page
       $("#articles").append("<p data-id='" + data[i].title + "'>" + data[i].summary + "<br />" + data[i].link + "</p>");
     }
   });
+  
+
+  // When the #clear-all button is pressed
+$("#clear-all").on("click", function() {
+  // Make an AJAX GET request to delete the notes from the db
+  $.ajax({
+    type: "GET",
+    dataType: "json",
+    url: "/clearall",
+    // On a successful call, clear the #results section
+    success: function(response) {
+      $("#results").empty();
+    }
+  });
+});
+
   
